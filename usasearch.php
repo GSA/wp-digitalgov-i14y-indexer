1286bfc585c677a1a415f21b882ac0f926ea81d0
Joshua K. Farrar <joshua@sent1nel.me>
2015-06-18 00:15:28 -0400

    basic wordpress plugin structure

---
+++ usasearch.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WP_USASearch
+ * @version 0.1
+ */
+/*
+Plugin Name: GSA OCSIT Hosted Search for Wordpress
+Plugin URI: https://github.com/sent1nel/search
+Description: This is not just a plugin, it is a way of life.
+Author: Joshua K. Farrar
+Version: 0.1
+Author URI: http://sent1nel.me/
+*/
